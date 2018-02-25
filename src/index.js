89ef35e3d4306aba07274a9e42c917b692d87e75
Kyle Farnung <kfarnung@outlook.com>
2018-02-24 22:44:57 -0800

    Use puzzle-lib for braille

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Braille } from 'puzzle-lib';
 import './braille.css';
 
 function BrailleCell(props) {
@@ -8,7 +9,7 @@ function BrailleCell(props) {
       <svg className={props.className}>
         <circle cx="50%" cy="50%" r="20%" />
         Sorry, your browser does not support inline SVG.
-      </svg> 
+      </svg>
     </button>
   );
 }
@@ -18,49 +19,15 @@ function CommandButton(props) {
     <button className="command" onClick={props.onClick}>
       {props.value}
     </button>
-  );   
-}
-
-function ParseBraille(value, numberMode) {
-  switch (value) {
-    case 0b000001: return numberMode ? "1" : "A";
-    case 0b000101: return numberMode ? "2" : "B";
-    case 0b000011: return numberMode ? "3" : "C";
-    case 0b001011: return numberMode ? "4" : "D";
-    case 0b001001: return numberMode ? "5" : "E";
-    case 0b000111: return numberMode ? "6" : "F";
-    case 0b001111: return numberMode ? "7" : "G";
-    case 0b001101: return numberMode ? "8" : "H";
-    case 0b000110: return numberMode ? "9" : "I";
-    case 0b001110: return numberMode ? "0" : "J";
-    case 0b010001: return "K";
-    case 0b010101: return "L";
-    case 0b010011: return "M";
-    case 0b011011: return "N";
-    case 0b011001: return "O";
-    case 0b010111: return "P";
-    case 0b011111: return "Q";
-    case 0b011101: return "R";
-    case 0b010110: return "S";
-    case 0b011110: return "T";
-    case 0b110001: return "U";
-    case 0b110101: return "V";
-    case 0b101110: return "W";
-    case 0b110011: return "X";
-    case 0b111011: return "Y";
-    case 0b111001: return "Z";
-    case 0b111010: return "#";
-
-    default: return "?";
-  }
+  );
 }
 
 class BrailleCharacter extends React.Component {
-  renderCell(i) {
+  renderCell(mask) {
     return (
       <BrailleCell
-        className={(this.props.bits & (1 << i)) !== 0 ? "selected" : "unselected"}
-        onClick={() => this.props.onClick(i)}
+        className={(this.props.bits & mask) !== 0 ? "selected" : "unselected"}
+        onClick={() => this.props.onClick(mask)}
       />
     );
   }
@@ -69,16 +36,16 @@ class BrailleCharacter extends React.Component {
     return (
       <div>
         <div className="braille-row">
-          {this.renderCell(0)}
-          {this.renderCell(1)}
+          {this.renderCell(Braille.Dot.UpperLeft)}
+          {this.renderCell(Braille.Dot.UpperRight)}
         </div>
         <div className="braille-row">
-          {this.renderCell(2)}
-          {this.renderCell(3)}
+          {this.renderCell(Braille.Dot.MiddleLeft)}
+          {this.renderCell(Braille.Dot.MiddleRight)}
         </div>
         <div className="braille-row">
-          {this.renderCell(4)}
-          {this.renderCell(5)}
+          {this.renderCell(Braille.Dot.LowerLeft)}
+          {this.renderCell(Braille.Dot.LowerRight)}
         </div>
       </div>
     );
@@ -89,47 +56,54 @@ class BrailleStream extends React.Component {
   constructor(props) {
     super(props);
 
+    this.stream = new Braille.Stream();
+    this.character = new Braille.Character();
+
     this.state = {
-      bits: 0,
-      content: "",
-      currentCharacter: "?",
-      numberMode: false
+      bits: this.character.valueOf(),
+      content: this.stream.toString(),
+      currentCharacter: '?'
     };
   }
 
-  handleClick(i) {
-    let bits = this.state.bits ^ (1 << i);
+  handleClick(mask) {
+    this.character.toggle(mask);
 
     this.setState({
-      bits: bits,
-      currentCharacter: ParseBraille(bits, this.state.numberMode)
+      bits: this.character.valueOf(),
+      currentCharacter: this.character.toString() || '?'
     });
   }
 
   handleNext() {
-    let current = this.state.currentCharacter;
-    let number = (current === "#") ? true : this.state.numberMode;
+    if (!this.character.empty()) {
+      this.stream.append(this.character);
+      this.character.clear();
 
-    this.setState({
-      content: this.state.content + current,
-      bits: 0,
-      currentCharacter: "?",
-      numberMode: number
-    });
+      this.setState({
+        bits: this.character.valueOf(),
+        content: this.stream.toString(),
+        currentCharacter: '?'
+      });
+    }
   }
 
   handleReset() {
+    this.character.clear();
+    this.stream.clear();
+
     this.setState({
-      bits: 0,
-      currentCharacter: "?",
-      content: ""
-    })
+      bits: this.character.valueOf(),
+      content: this.stream.toString(),
+      currentCharacter: '?'
+    });
   }
 
   handleSpace() {
+    this.stream.space();
+
     this.setState({
-      content: this.state.content + " ",
-      numberMode: false
+      content: this.stream.toString()
     });
   }
 
@@ -148,7 +122,7 @@ class BrailleStream extends React.Component {
           <CommandButton value="Next" onClick={() => this.handleNext()} />
           <CommandButton value="Space" onClick={() => this.handleSpace()} />
           <CommandButton value="Reset" onClick={() => this.handleReset()} />
-        </div>  
+        </div>
         <div className="character-stream">{this.state.content}</div>
       </div>
     );
