fed8299a4ae64dcdfd0cc72a954bb409a1b95c77
Brian Beck <brian.beck@outlook.com>
2018-08-19 03:46:03 +0200

    Add Morse Reference (#67)
    * Add Morse Reference
    
    * Move Morse SVG into its own component

---
+++ src/Morse/MorseTable.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { MorseCharacter as Character } from 'puzzle-lib';
+import { Table } from 'react-bootstrap';
+import MorsePicture from './MorsePicture';
+import './MorseTable.css';
+
+class MorseTable extends React.Component {
+  public render() {
+    return (
+      <div className="MorseTable">
+        <Table striped={true} responsive={true}>
+          <thead>
+            <tr>
+              <th>Character</th>
+              <th>Morse</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderRows()}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+
+  private renderRows() {
+    const characters = new Character().getPotentialMatches();
+    return characters.map((entry) => (
+      <tr key={entry.display}>
+        <td>{entry.display}</td>
+        <td>
+          <div className="MorseTable-morseView">
+            <MorsePicture morseString={Character.toMorseString(entry.encoding)} />
+          </div>
+        </td>
+      </tr>
+    ));
+  }
+}
+
+export default MorseTable;
--- src/Views/Encoding/Morse.tsx
+++ src/Views/Encoding/Morse.tsx
@@ -1,16 +1,36 @@
 import * as React from 'react';
+import { Tab, Tabs } from 'react-bootstrap';
+import TabViewBase from '../TabViewBase';
 import MorseStream from '../../Morse/MorseStream';
+import MorseTable from '../../Morse/MorseTable';
 import './Morse.css';
 
-class Morse extends React.Component {
+class Morse extends TabViewBase {
   public render() {
     return (
       <div className="Morse">
         <div className="Morse-content">
-          <MorseStream />
+          <Tabs
+            activeKey={this.state.activeKey}
+            animation={false}
+            id="Morse-tabs"
+            // tslint:disable-next-line: no-any
+            onSelect={(activeKey: any) => this.onTabSelect(activeKey as number)}
+          >
+            <Tab eventKey={1} title="Value">
+              <MorseStream />
+            </Tab>
+            <Tab eventKey={2} title="Reference">
+              <MorseTable />
+            </Tab>
+          </Tabs>
         </div>
       </div>
     );
+  }
+
+  protected getLocalStorageKey() {
+    return 'Morse';
   }
 }
 
