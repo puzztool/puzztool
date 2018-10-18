3ea355f2090b405a72316079ae41e7c21075282f
Brian Beck <brian.beck@outlook.com>
2018-10-18 03:00:12 +0000

    Add NATO Reference table

---
+++ src/Reference/NatoTable.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { NatoCharacter, NatoData } from 'puzzle-lib';
+import { Table } from 'react-bootstrap';
+
+class NatoTable extends React.Component {
+  public render() {
+    return (
+        <Table striped={true} responsive={true}>
+          <thead>
+            <tr>
+              <th>Letter</th>
+              <th>NATO Word</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderRows(NatoData.instance.entries)}
+          </tbody>
+        </Table>
+    );
+  }
+
+  private renderRows(data: NatoCharacter[]) {
+    return data.map((value: NatoCharacter) => {
+      return (
+        <tr key={value.character}>
+          <td>{value.character}</td>
+          <td>{value.word}</td>
+        </tr>
+      );
+    });
+  }
+}
+
+export default NatoTable;
