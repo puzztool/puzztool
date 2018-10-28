c0d5f6f39293925c7c6a48b5a1def11ef998f176
Brian Beck <brian.beck@outlook.com>
2018-10-27 19:24:47 -0700

    Add Semaphore Completion

--- src/Semaphore/SemaphoreCharacter.tsx
+++ src/Semaphore/SemaphoreCharacter.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import SemaphoreCheckbox from './SemaphoreCheckbox';
+import SemaphoreCompletion from './SemaphoreCompletion';
 import SemaphorePicture from './SemaphorePicture';
 import { SemaphoreCharacter as Character, SemaphoreDirection as Direction } from 'puzzle-lib';
 import './SemaphoreCharacter.css';
@@ -19,51 +20,92 @@ class SemaphoreCharacter extends React.Component<Props> {
       <div className="SemaphoreCharacter">
         <SemaphoreCheckbox
           character={this.props.character}
-          className="SemaphoreCharacter-north"
+          className="SemaphoreCheckbox-north"
           direction={Direction.North}
           onChange={this.props.onChange}
         />
         <SemaphoreCheckbox
           character={this.props.character}
-          className="SemaphoreCharacter-northEast"
+          className="SemaphoreCheckbox-northEast"
           direction={Direction.NorthEast}
           onChange={this.props.onChange}
         />
         <SemaphoreCheckbox
           character={this.props.character}
-          className="SemaphoreCharacter-east"
+          className="SemaphoreCheckbox-east"
           direction={Direction.East}
           onChange={this.props.onChange}
         />
         <SemaphoreCheckbox
           character={this.props.character}
-          className="SemaphoreCharacter-southEast"
+          className="SemaphoreCheckbox-southEast"
           direction={Direction.SouthEast}
           onChange={this.props.onChange}
         />
         <SemaphoreCheckbox
           character={this.props.character}
-          className="SemaphoreCharacter-south"
+          className="SemaphoreCheckbox-south"
           direction={Direction.South}
           onChange={this.props.onChange}
         />
         <SemaphoreCheckbox
           character={this.props.character}
-          className="SemaphoreCharacter-southWest"
+          className="SemaphoreCheckbox-southWest"
           direction={Direction.SouthWest}
           onChange={this.props.onChange}
         />
         <SemaphoreCheckbox
           character={this.props.character}
-          className="SemaphoreCharacter-west"
+          className="SemaphoreCheckbox-west"
           direction={Direction.West}
           onChange={this.props.onChange}
         />
         <SemaphoreCheckbox
           character={this.props.character}
-          className="SemaphoreCharacter-northWest"
+          className="SemaphoreCheckbox-northWest"
           direction={Direction.NorthWest}
           onChange={this.props.onChange}
+        />
+        
+        <SemaphoreCompletion
+          character={this.props.character}
+          className="SemaphoreCompletion-north"
+          direction={Direction.North}
+        />
+        <SemaphoreCompletion
+          character={this.props.character}
+          className="SemaphoreCompletion-northEast"
+          direction={Direction.NorthEast}
+        />
+        <SemaphoreCompletion
+          character={this.props.character}
+          className="SemaphoreCompletion-east"
+          direction={Direction.East}
+        />
+        <SemaphoreCompletion
+          character={this.props.character}
+          className="SemaphoreCompletion-southEast"
+          direction={Direction.SouthEast}
+        />
+        <SemaphoreCompletion
+          character={this.props.character}
+          className="SemaphoreCompletion-south"
+          direction={Direction.South}
+        />
+        <SemaphoreCompletion
+          character={this.props.character}
+          className="SemaphoreCompletion-southWest"
+          direction={Direction.SouthWest}
+        />
+        <SemaphoreCompletion
+          character={this.props.character}
+          className="SemaphoreCompletion-west"
+          direction={Direction.West}
+        />
+        <SemaphoreCompletion
+          character={this.props.character}
+          className="SemaphoreCompletion-northWest"
+          direction={Direction.NorthWest}
         />
         <SemaphorePicture
           width={240}
