fd7a978326e8fb9bc33b4f0f9655937370db0c20
Scott Jenks <sjenks@gmail.com>
2018-11-04 10:40:09 -0800

    Address code review comments
    -Split url into root and child
    -Break out description from category

--- src/Data/RouteData.ts
+++ src/Data/RouteData.ts
@@ -8,77 +8,83 @@ export class RouteData {
             'name': 'Puzztool',
             'categories': [
                 {
-                    'name': 'Ciphers - Scrambled Letters',
+                    'name': 'Ciphers',
+                    'description': 'Scrambled letters',
+                    'rootUrl': '/cipher',
                     'children': [
                         {
                             'name': 'Caesar',
-                            'url': '/cipher/caesar',
+                            'url': '/caesar',
                             'description': 'Letters are shifted by a fixed value'
                         },
                         {
                             'name': 'Vignere',
-                            'url': '/cipher/vigenere',
+                            'url': '/vigenere',
                             'description': 'Letters are mapped based on a key word'
                         },
                         {
                             'name': 'Caesar',
-                            'url': '/cipher/autokey',
+                            'url': '/autokey',
                             'description': ' A Vignere cipher which uses plaintext as the key'
                         }
                     ]
                 },
                 {
-                    'name': 'Encoding - Different Ways To Represent Letters',
+                    'name': 'Encodings',
+                    'description': 'Different Ways To Represent Letters',
+                    'rootUrl': '/encoding',
                     'children': [
                         {
                             'name': 'AutoConvert',
-                            'url': '/encoding/autoconvert',
+                            'url': '/autoconvert',
                             'description': 'Mapping numbers to letters (ASCII, Binary, Ordinal)'
                         },
                         {
                             'name': 'Braille',
-                            'url': '/encoding/braille',
+                            'url': '/braille',
                             'description': 'Six dots in a two-by-three grid'
                         },
                         {
                             'name': 'Morse Code',
-                            'url': '/encoding/morse',
+                            'url': '/morse',
                             'description': 'Dots and dashes with separations'
                         },
                         {
                             'name': 'Semaphore',
-                            'url': '/encoding/semaphore',
+                            'url': '/semaphore',
                             'description': ' Positions of two different flags'
                         },
                         {
                             'name': 'Pigpen',
-                            'url': '/encoding/pigpen',
+                            'url': '/pigpen',
                             'description': 'Parts of a tic-tac-toe grid or X, some with dot'
                         }
         
                     ]
                 },
                 {
-                    'name': 'Reference - Misc information',
+                    'name': 'Reference',
+                    'description': 'Misc information',
+                    'rootUrl': '/reference',
                     'children': [
                         {
                             'name': 'Character Encodings',
-                            'url': '/reference/characterencodings',
+                            'url': '/characterencodings',
                             'description': 'Static display of number to letter mapping'
                         },
                         {
                             'name': 'NATO Alphabet',
-                            'url': '/reference/nato',
+                            'url': '/nato',
                             'description': 'Words used to clearly identify a letter'
                         },
                         {
                             'name': 'Naval Flags',
-                            'url': '/reference/navalflags',
+                            'url': '/navalflags',
                             'description': 'Different simple flags which convey a letter'
                         },
                         {
                             'name': 'Resistors',
-                            'url': '/reference/resistors',
+                            'url': '/resistors',
                             'description': 'Find a resistor value using different colored bands'
                         }
                     ]
--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -18,15 +18,15 @@ class Home extends React.Component {
     const categoryData = RouteData.getCategories();
     var markup = [];
     for (const category of categoryData) {
-      markup.push(
-        <h2>{category.name}</h2>
-      );
+      markup.push(<h2>{category.name}</h2>);
+      markup.push(<h4>{category.description}</h4>);
       
       for (const child of category.children) {
+        const fullUrl = category.rootUrl + child.url;
         markup.push(
           (
           <li>
-            <Link to={child.url}>{child.name}</Link> - {child.description}
+            <Link to={fullUrl}>{child.name}</Link> - {child.description}
           </li>
         )
         );
