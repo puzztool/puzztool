e05948850cab2ad8f0ba1797f875c4fc29fd08fa
Brian Beck <brian.beck@outlook.com>
2019-03-31 13:50:22 -0700

    Clear local storage when version increases

--- src/Data/LocalStorageComponent.ts
+++ src/Data/LocalStorageComponent.ts
@@ -1,7 +1,30 @@
+import compareVersions from 'compare-versions';
 import { Component } from 'react';
 import LocalStorage from './LocalStorage';
+import { version } from '../version';
 
 abstract class LocalStorageComponent<P = {}, S = {}, SavedState = {}> extends Component<P, S> {
+  public constructor(props: P) {
+    super(props);
+
+    // If the current localstorage was created with a prior version of puzztool, clear
+    // the local storage to get rid of any obsolete or incompatible artifacts
+    const previousVersion = LocalStorage.getObject<string>(this.VERSION_STORAGE_KEY);
+    if (this.versionIncreased(previousVersion, version)) {
+      LocalStorage.clear();
+      // Store the current version
+      LocalStorage.setObject<string>(this.VERSION_STORAGE_KEY, version);
+    }
+  }
+
+  private versionIncreased(prev: string | null, current: string): boolean {
+    if (prev == null) {
+      return true;
+    }
+
+    return compareVersions(current, prev) === 1;
+  }
+
   public componentDidMount() {
     this.restoreState();
     this.updateState();
@@ -24,6 +47,8 @@ abstract class LocalStorageComponent<P = {}, S = {}, SavedState = {}> extends Co
   private restoreState() {
     this.onRestoreState(LocalStorage.getObject<SavedState>(this.getLocalStorageKey()));
   }
+
+  private readonly VERSION_STORAGE_KEY: string = "puzztoolVersion";
 }
 
 export default LocalStorageComponent;
