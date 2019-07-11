439b64d1a17b6bb1895f1563c71db07651639263
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-11 06:41:32 +0000

    Deploy puzztool.com to github.com/puzztool/puzztool.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec425a1d0f4da49f3e0300a230d14a84.js"
+  "/precache-manifest.2b24959c47a17565f03a4dd856aeff5e.js"
 );
 
 self.addEventListener('message', (event) => {
