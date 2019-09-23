f51e55404ae58fc4908938aa0995ea2a2eed2539
Kyle Farnung <kfarnung@outlook.com>
2019-09-22 23:13:00 -0700

    Run the initial load synchronously

--- src/Data/LocalStorageHooks.ts
+++ src/Data/LocalStorageHooks.ts
@@ -1,14 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import LocalStorage from './LocalStorage';
 
 export function useLocalStorage<T>(
   localStorageKey: string,
   onRestoreState: (state: T | null) => void,
   onSaveState: () => T) {
-  // This effect only runs on the first mount of the control or whenever the
-  // `localStorageKey` value changes. `onRestoreState` is intentionally omitted
-  // since each call will have a unique version of the function.
-  useEffect(
+  // This effect runs synchronously on the first mount of the control or
+  // whenever the `localStorageKey` value changes. This is to ensure that there
+  // is no flickering as the data is rendered. `onRestoreState` is intentionally
+  // omitted from deps since each call will have a unique version of the
+  // function.
+  useLayoutEffect(
     () => onRestoreState(LocalStorage.getObject<T>(localStorageKey)),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [localStorageKey]);
