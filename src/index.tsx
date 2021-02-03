40ca7bf15774ddf83a4958364c22df2b712fa036
Kyle Farnung <kfarnung@outlook.com>
2021-02-02 22:09:43 -0800

    Fix service worker (#202)
    CRA v4 moved the service worker out of the core and into a template.
    Incorporate the template into the app.
    
    https://github.com/facebook/create-react-app/issues/9776

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,39 @@
-import { createBrowserHistory } from 'history';
-import ReactDOM from 'react-dom';
-import { PersistGate } from 'redux-persist/integration/react'
-import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import configureStore from './Store/configureStore';
-import './index.scss';
+import { createBrowserHistory } from "history";
+import React from "react";
+import ReactDOM from "react-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import configureStore from "./Store/configureStore";
+import "./index.scss";
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') || '';
+const baseUrl =
+  document.getElementsByTagName("base")[0].getAttribute("href") || "";
 const history = createBrowserHistory({ basename: baseUrl });
 const { store, persistor } = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <Router history={history}>
-        <App />
-      </Router>
-    </PersistGate>
-  </Provider>,
-  document.getElementById('root'));
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <App />
+        </Router>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-serviceWorker.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
