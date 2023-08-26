5eb8cf90face2a592191212df32461ff46024e49
Kyle Farnung <kfarnung@outlook.com>
2023-08-26 11:42:44 -0700

    Convert to Vite

---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
+import svgrPlugin from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+  },
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    VitePWA({
+      filename: "service-worker.js",
+      manifest: {
+        icons: [
+          {
+            src: "icons-64-fill.png",
+            sizes: "64x64",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icons-192-fill.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icons-512-fill.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        name: "PuzzTool",
+        short_name: "PuzzTool",
+      },
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text', 'html'],
+      exclude: [
+        'node_modules/',
+      ],
+    },
+  },
+});
