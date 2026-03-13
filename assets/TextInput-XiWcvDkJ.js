4435b35ded52f372cfdceb3ef20bc4859e2d2592
kfarnung <kfarnung@users.noreply.github.com>
2026-03-13 03:01:29 +0000

    deploy: 94915528006fca6d00df2c177f64fd95a8703829

---
+++ assets/TextInput-XiWcvDkJ.js
@@ -0,0 +1,1 @@
+import{q as p,u as a,j as r}from"./index-DNQ1xmYo.js";import{I as t}from"./FocusInput-CYx9dJvQ.js";const s=p((e,n)=>{const o=a("TextInput",null,e);return r.jsx(t,{component:"input",ref:n,...o,__staticSelector:"TextInput"})});s.classes=t.classes;s.displayName="@mantine/core/TextInput";export{s as T};
